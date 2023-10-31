185517478869af17232c4a492b3e92d464c2f8e7
Invento-R7 <david.fc@inventoteca.com>
2023-10-31 02:35:47 -0600

    master_module

---
+++ master_gas_module/mqttservice.h
@@ -0,0 +1,37 @@
+#ifndef MQTTSERVICE_H
+#define MQTTSERVICE_H
+
+#include "system.h"
+
+extern WiFiClient espClient;
+extern PubSubClient Mclient;
+extern WiFiClient client_http;
+
+extern const char* publish_topic; 
+extern const char* subcribe_topic; 
+
+extern const char* list_topic;
+extern const char* add_topic;
+extern const char* config_topic;
+extern const char* wild_topic;
+extern const char* gps_topic;
+extern const char* log_topic;
+
+extern char buffer_union_publish[30]; 
+extern char buffer_union_subcribe[30];
+extern char buffer_msg[1024];
+extern const char* client_id;
+
+extern volatile boolean send_log;
+extern volatile boolean clear_log;
+extern volatile boolean new_log;
+
+void callback(char* topic, byte* payload, unsigned int length);
+bool reconnect();
+void mqtt_init();
+bool mqtt_check();
+void mqtt_send();
+
+
+
+#endif  //
---
+++ master_gas_module/system.h
@@ -0,0 +1,59 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+//#include <vector>
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include <LittleFS.h>
+//#include <Wire.h>
+#include "i2c_fifo.h"
+#include "i2c_slave.h"
+#include "pico/stdlib.h"
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include "time.h"
+#include "RTClib.h"
+#include <Wire.h>
+#include <TinyGPSPlus.h>
+
+//#include <WiFiManager_RP2040W.h>  
+
+#include "version.h"
+#include "wifiservice.h"
+//#include "pins.h"
+#include "filespiffs.h"
+#include "mqttservice.h"
+#include "wireservice.h"
+#include "clock.h"
+#include "gps_service.h"
+
+
+//15 seconds WDT
+#define WDT_TIMEOUT 15
+
+extern bool factory_press;
+extern unsigned long factory_time;
+extern unsigned long prev_factory_time;
+//extern bool reset_time;
+//extern byte localAddress;     // address of this device
+extern unsigned long mainRefresh;
+extern unsigned long mainTime;
+extern const uint32_t connectTimeoutMs;
+extern unsigned long  s_timestamp;
+extern int buttonState;
+extern volatile bool found_client;
+//extern volatile uint32_t nclient_data; // nclient_data[4]
+//extern volatile uint8_t price_data[2], litro_data[4], factor_data[2], name_data[42];
+
+//void IRAM_ATTR factory_reset3();
+void reset_config();
+//void check_reset();
+bool strToBool(String str);
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max);
+void loadConfig();
+void system_init();
+void search_nclient(uint32_t aux_client);
+void register_client();
+#endif
