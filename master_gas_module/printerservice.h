090e9cbc95c8762362a98991f905cfe8b28f11e0
Invento-R7 <david.fc@inventoteca.com>
2023-11-07 10:37:58 -0600

    test report

--- master_gas_module/printerservice.h
+++ master_gas_module/printerservice.h
@@ -3,6 +3,7 @@
 
 #include "system.h"
 
+void printReport(uint32_t num, uint32_t ltr, uint32_t unitprice, uint8_t d, uint8_t m, uint8_t y, uint8_t h, uint8_t mn, uint8_t f);
 void printCheck(uint32_t num, uint32_t ltr, uint32_t unitprice, uint8_t d, uint8_t m, uint8_t y, uint8_t h, uint8_t mn, uint8_t f);
 void printing_logs();
 
