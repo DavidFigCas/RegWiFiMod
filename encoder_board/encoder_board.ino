ed969b60765820320cef227b6eb480de0b99e516
Invento-R7 <david.fc@inventoteca.com>
2024-05-18 14:03:01 -0600

    encoder wip

--- encoder_board/encoder_board.ino
+++ encoder_board/encoder_board.ino
@@ -1,10 +1,12 @@
-#define MAX_DELTA   1      //10 Pulsos detectados en 500ms (Intervalo2)
+
+
 #define LED_1      25
 #define LED_2     27
 #define LED_3     28
 #define BTN_START   12
 #define BTN_STOP    11
 #define SOLENOID    10
+#define FILE_SIZE   512
 
 #define BUF_LEN         0x100
 #define I2C_SLAVE_ADDRESS 0x5C
@@ -20,6 +22,7 @@
 #include "hardware/timer.h"
 #include <ArduinoJson.h>
 #include <Wire.h>
+#include <LittleFS.h>
 //#include "RP2040_TimerInterrupt.h"
 
 static char buff[200];
@@ -28,6 +31,19 @@ String jsonStr;
 
 StaticJsonDocument<200> doc;  // Asegúrate de que el tamaño sea suficiente para tu objeto JSON
 StaticJsonDocument<200> doc_aux;  // Crea un documento JSON con espacio para 200 bytes
+JsonObject obj_conf;
+StaticJsonDocument<FILE_SIZE> doc_conf;
+const char* filename = "/config.json";
+volatile bool saveConfig = false;
+File file;
+
+// ------------------------------ Configs
+unsigned long interval = 500;              // Print and send
+unsigned long MAX_DELTA = 1;                //10 Pulsos detectados en Intervalo2  (DELTA)
+unsigned long intervalo = 100;    //ENCODER Intervalo de tiempo (milisegundos)
+unsigned long intervalo2 = 2000;  //DELTA Intervalo de tiempo (500 milisegundos)
+unsigned long noDelta_timeSTOP = 60;// Maximo tiempo desde que se detecto STOP_FLOW 60=30seg
+
 
 uint8_t STATE = 0;
 volatile int32_t current_value;
@@ -35,22 +51,22 @@ volatile int32_t target_value;
 volatile int32_t new_value, delta, old_value = 0;
 bool flow, buttonx, act_button = false;
 unsigned long previousMillis = 0;  // Almacena la última vez que el LED cambió
-const long interval = 500;
+
 unsigned long currentMillis;
 
 PioEncoder encoder(2); // encoder is connected to GPIO2 and GPIO3
 uint64_t alarm_callback(alarm_id_t id, void *user_data);
 
-const unsigned long intervalo = 100;  // Intervalo de tiempo (1 minuto en milisegundos)
+
 unsigned long tiempoAnterior = 0;
 unsigned long tiempoActual;
 
-const unsigned long intervalo2 = 2000;  // Intervalo de tiempo (500 milisegundos)
+
 unsigned long tiempoAnterior2 = 0;
 unsigned long tiempoActual2;
 
-unsigned long noDelta_timeSTOP = 60;// Maximo tiempo desde que se detecto STOP_FLOW 60=30seg
-unsigned long noDelta_timeCounter = 0;// Maximo tiempo desde que se detecto STOP_FLOW
+
+unsigned long noDelta_timeCounter = 0;// Contador tiempo desde que se detecto STOP_FLOW
 
 
 
@@ -76,7 +92,9 @@ void setup()
   digitalWrite(28, 0);
   pinMode(27, OUTPUT);
   digitalWrite(27, 0);
-  //delay(2000);
+
+  delay(2000);
+
   Serial.println("I2C Ready");
   //gpio_set_irq_enabled_with_callback(BTN_START, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
   pinMode(SOLENOID, OUTPUT);
@@ -93,7 +111,10 @@ void setup()
   // doc["valve_open"] = false;
   close_valve();
 
-
+  if (spiffs_init())
+  {
+    //loadConfig();       // Load and update behaivor of system
+  }
 }
 
 
@@ -238,6 +259,7 @@ void loop()
             Serial.println("AUTO STOP");
           }
           close_valve();
+          target_value = 0;
           //STATE = 2;
         }
 
@@ -389,10 +411,149 @@ void close_valve()
 }
 
 
-uint64_t alarm_callback(alarm_id_t id, void *user_data) {
+uint64_t alarm_callback(alarm_id_t id, void *user_data)
+{
   // Cambiar el estado del LED cada vez que se llama a la interrupción
   //digitalWrite(LED_PIN, !digitalRead(LED_PIN));
 
   // Devolver 0 para que el timer siga llamando a esta función
   return 0;
+}
+
+
+// ------------------------------------------------------------------------------------- spiffs_init
+bool spiffs_init()
+{
+  if (!LittleFS.begin())
+  {
+    Serial.println("{\"spiffs\":false}");
+    return false;
+  }
+  else
+  {
+    Serial.println("{\"spiffs\":true}");
+    listFiles();
+    Cfg_get(/*NULL*/);  // Load File from spiffs
+    return true;
+  }
+}
+
+// --------------------------------------------------------------------------------------------------- Cfg_get
+void Cfg_get()
+//  {"method":"Config.Get"}
+{
+  // open file to load config
+
+  obj_conf = getJSonFromFile(&doc_conf, filename);
+
+
+
+
+  if (obj_conf.size() == 0)
+  {
+    Serial.println("{\"config_file\":\"empty\"}");
+    //doc_conf = getJSonFromFile(&doc, filedefault);
+    //Serial.println(saveJSonToAFile(&obj, filename) ? "{\"file_default_restore\":true}" : "{\"file_default_restore\":false}");
+  }
+
+  //if (obj["test"].as<bool>() == true)
+  {
+    // Comment for production
+    serializeJson(doc_conf, Serial);
+    Serial.println();
+  }
+
+
+
+}
+
+
+// ------------------------------------------------------------------------------------------------ getJsonFromFile
+
+JsonObject getJSonFromFile(StaticJsonDocument<FILE_SIZE> *doc_conf, String filename)
+{
+
+
+  file = LittleFS.open(filename, "r");
+  if (file)
+  {
+    Serial.println("Opening File");
+
+    size_t size = file.size();
+    Serial.println(size);
+
+    if (size > FILE_SIZE)
+    {
+      Serial.println("Too large file");
+
+    }
+
+    DeserializationError error = deserializeJson(*doc_conf, file);
+    if (error)
+    {
+      // if the file didn't open, print an error:
+      //Serial.print(F("Error parsing JSON "));
+      //Serial.println(error.c_str());
+
+      // if (forceCleanONJsonError)
+      {
+        Serial.println("{\"force_empty_json\": true}");
+        return doc_conf->to<JsonObject>();
+      }
+    }
+
+    // close the file:
+    file.close();
+    Serial.println("{\"json_loaded\": true}");
+
+    return doc_conf->as<JsonObject>();
+  } else {
+    // if the file didn't open, print an error:
+    //Serial.print(F("Error opening (or file not exists) "));
+    //Serial.println(filename);
+
+    //Serial.println(F("Empty json created"));
+    Serial.println("{\"empty_json\": true}");
+    return doc_conf->to<JsonObject>();
+  }
+
+}
+
+// ------------------------------------------------------------------------ListFiles
+void listFiles() {
+  // Abre el directorio raíz
+
+  // open the file for reading:
+  char buff[32];
+  int cnt = 1;
+  File f = LittleFS.open("config.json", "r");
+  if (f) {
+    bzero(buff, 32);
+    if (f.read((uint8_t *)buff, 31)) {
+      sscanf(buff, "%d", &cnt);
+      Serial.printf("I have been run %d times\n", cnt);
+    }
+    f.close();
+  }
+
+  Serial.println("Listing Files");
+
+  Dir dir = LittleFS.openDir("/");
+
+  // Recorre los archivos en el directorio raíz
+  while (dir.next()) {
+    Serial.print("File: ");
+    Serial.print(dir.fileName());
+    Serial.print("\tSize: ");
+    Serial.println(dir.fileSize());
+  }
+}
+
+
+// ---------------------------------------------------------------------------------------------------- loadConfig
+// Update a new config in the file an change behivor
+void loadConfig()
+{
+  // ----------- Load Counters
+  Serial.println("{\"loadConfig\":true}");
 }
